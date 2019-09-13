db96757523eae473c3c343c7f8436a43155485c2
anloffredo <loffredo.ap@gmail.com>
2019-09-12 20:03:47 -0700

    fixing "hasOptional" and adding "mapped" attribute for non-public properties
    fixing "hasOptional" and adding "mapped" attribute for non-public properties

--- Attributes/Relations/HasOptional.cs
+++ Attributes/Relations/HasOptional.cs
@@ -3,29 +3,41 @@
 namespace Penguin.Persistence.Abstractions.Attributes.Relations
 {
     /// <summary>
-    /// Denotes that this relationship should be optional, which should be the default
+    /// Specifies that the property this applies to is optional
+    /// be set
     /// </summary>
-    public class HasOptionalAttribute : RelationalAttribute
+    public class HasOptionalAttribute : MappingAttribute
     {
         /// <summary>
-        /// The property name that defines the key referenced
+        /// Constructs a new instance of this attribute
         /// </summary>
-        public string TargetProperty { get; set; }
-
-        /// <summary>
-        /// The type of the object that this property (assumed key) references
-        /// </summary>
-        public Type TargetType { get; set; }
+        public HasOptionalAttribute()
+        {
+            this.SetMapping = new Mapping()
+            {
+            };
+        }
 
         /// <summary>
         /// Constructs a new instance of this attribute
         /// </summary>
-        /// <param name="targetProperty">The property name that defines the key referenced</param>
-        /// <param name="targetType">The type of the object that this property (assumed key) references</param>
-        public HasOptionalAttribute(string targetProperty, Type targetType = null)
+        /// <param name="rightProperty">The optional name for the property that links back to this object</param>
+        public HasOptionalAttribute(string rightProperty)
         {
-            TargetProperty = targetProperty;
-            TargetType = targetType;
+            this.SetMapping = new Mapping()
+            {
+                Right = new End()
+                {
+                    Property = rightProperty
+                }
+            };
         }
+
+        /// <summary>
+        /// Returns the expected type of the property that links back to this one
+        /// </summary>
+        /// <param name="LeftPropertyType">The type of the class containing this attribute. Its a one-to-one so it just returns this type</param>
+        /// <returns>The type of the class containing this attribute</returns>
+        public override Type GetRightPropertyType(Type LeftPropertyType) => LeftPropertyType;
     }
 }
---
+++ Attributes/Relations/Mapped.cs
@@ -0,0 +1,9 @@
+﻿namespace Penguin.Persistence.Abstractions.Attributes.Relations
+{
+    /// <summary>
+    /// Put on a private property to specify that it SHOULD be mapped
+    /// </summary>
+    public class MappedAttribute : PersistenceAttribute
+    {
+    }
+}
