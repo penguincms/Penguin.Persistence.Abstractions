b72f99c1f61af4fe71e0a8a828547bb931e1f04a
anloffredo <loffredo.ap@gmail.com>
2019-09-16 18:49:38 -0700

    Adding clone to interfaces
    Adding clone to interfaces

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -6,23 +6,7 @@ using System.Threading.Tasks;
 
 namespace Penguin.Persistence.Abstractions.Interfaces
 {
-    /// <summary>
-    /// An interface intended to allow for access to a persistence context of a given type
-    /// </summary>
-    /// <typeparam name="T">The type of data used in the persistence context</typeparam>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "<Pending>")]
-    public interface IRepository<T> : IRepositoryI<T>, IRepositoryO<T>, IRepository, IQueryable<T> where T : class
-    {
-        /// <summary>
-        /// The persistence context containing the data this repository accesses
-        /// </summary>
-        IPersistenceContext<T> Context { get; }
 
-        /// <summary>
-        /// An accessor for only derived types
-        /// </summary>
-        IQueryable<TDerived> OfType<TDerived>() where TDerived : T;
-    }
 
     /// <summary>
     /// A nongeneric representation of a repository allowing for access without knowing the underlying data type
@@ -95,6 +79,25 @@ namespace Penguin.Persistence.Abstractions.Interfaces
         /// </summary>
         /// <returns> a new write context for the underlying persistence context</returns>
         IWriteContext WriteContext();
+
+        /// <summary>
+        /// Resets the object unique fields and adds a copy to the context
+        /// </summary>
+        /// <param name="o">The object to copy and add</param>
+        void AddCopy(object o);
+
+        /// <summary>
+        /// Resets the object unique fields and adds or updates a copy to the context
+        /// </summary>
+        /// <param name="o">The object to copy and add</param>
+        void AddOrUpdateCopy(object o);
+
+        /// <summary>
+        /// Creates a shallow clone of an object with new keys
+        /// </summary>
+        /// <param name="o">The object to clone</param>
+        object ShallowClone(object o);
+
     }
 
     /// <summary>
---
+++ Interfaces/IRepositoryT.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Penguin.Persistence.Abstractions.Interfaces
+{
+    /// <summary>
+    /// An interface intended to allow for access to a persistence context of a given type
+    /// </summary>
+    /// <typeparam name="T">The type of data used in the persistence context</typeparam>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "<Pending>")]
+    public interface IRepository<T> : IRepositoryI<T>, IRepositoryO<T>, IRepository, IQueryable<T> where T : class
+    {
+        /// <summary>
+        /// The persistence context containing the data this repository accesses
+        /// </summary>
+        IPersistenceContext<T> Context { get; }
+
+        /// <summary>
+        /// An accessor for only derived types
+        /// </summary>
+        IQueryable<TDerived> OfType<TDerived>() where TDerived : T;
+
+        /// <summary>
+        /// Resets the object unique fields and adds a copy to the context
+        /// </summary>
+        /// <param name="o">The object to copy and add</param>
+        void AddCopy(T o);
+
+        /// <summary>
+        /// Resets the object unique fields and adds or updates a copy to the context
+        /// </summary>
+        /// <param name="o">The object to copy and add</param>
+        void AddOrUpdateCopy(T o);
+
+        /// <summary>
+        /// Creates a shallow clone of an object with new keys
+        /// </summary>
+        /// <param name="o">The object to clone</param>
+        T ShallowClone(T o);
+    }
+}
