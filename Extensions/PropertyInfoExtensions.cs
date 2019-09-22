e56b85c9d1bffdb93be860656a583df35497d3ba
anloffredo <loffredo.ap@gmail.com>
2019-09-22 10:41:54 -0700

    adding extension to get property name with override if set by display attribtue
    adding extension to get property name with override if set by display attribtue

---
+++ Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Penguin.Persistence.Abstractions.Attributes.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Reflection;
+using System.Text;
+
+namespace Penguin.Persistence.Abstractions.Extensions
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public static class PropertyInfoExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    {
+        /// <summary>
+        /// Attempts to get a name from a set DisplayAttribute, and if not found, falls back on the property name
+        /// </summary>
+        /// <param name="property">The property to retrieve the name from</param>
+        /// <returns>The proper display name for the property</returns>
+        public static string DisplayName(this PropertyInfo property)
+        {
+            Contract.Requires(property != null);
+
+            DisplayAttribute display = property.GetCustomAttribute<DisplayAttribute>();
+
+            if (string.IsNullOrWhiteSpace(display?.Name))
+            {
+                return property.Name;
+            }
+            else
+            {
+                return display.Name;
+            }
+        }
+    }
+}
